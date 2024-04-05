f3debf20f931f9e35607def2c09969edc7d4add3
Mesaque <63453751+titiushadow@users.noreply.github.com>
2024-04-05 17:29:16 -0300

    Tela de usuario e tela de perfil

---
+++ ClientWin32/Views/uFrmProdutos.pas
@@ -0,0 +1,34 @@
+unit uFrmProdutos;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uFrmModelo, Data.DB, Vcl.Menus,
+  System.Actions, Vcl.ActnList, System.ImageList, Vcl.ImgList, Vcl.Grids,
+  Vcl.DBGrids, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls, Vcl.ComCtrls,
+  Vcl.ToolWin, Vcl.Mask, Vcl.DBCtrls;
+
+type
+  TFrmProdutos = class(TFrmModelo)
+    Label1: TLabel;
+    DBEdit1: TDBEdit;
+    Label2: TLabel;
+    DBEdit2: TDBEdit;
+    Label4: TLabel;
+    DBEdit3: TDBEdit;
+    Label5: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmProdutos: TFrmProdutos;
+
+implementation
+
+{$R *.dfm}
+
+end.
